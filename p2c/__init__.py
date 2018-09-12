f158b8a0036b9a7548b1faebb85aeda37b29a50d
Matt Bressler <matth.bressler@gmail.com>
2018-09-11 20:06:53 -0400

    adding logging statement

--- p2c/__init__.py
+++ p2c/__init__.py
@@ -120,7 +120,7 @@ log.addHandler(h)
 
 
 def create_app(test_config=None):
-
+    print("creating app")
     # create and configure the app
     app = Flask(__name__, instance_relative_config=True)
     app.config.from_mapping(
